d17122dd214bd99f45c5968304014de30a200d06
William Verde <williamverde10@gmail.com>
2019-10-25 16:16:25 -0400

    Add: manage resource permission

--- Config/permissions.php
+++ Config/permissions.php
@@ -2,24 +2,28 @@
 
 return [
     'iredeems.items' => [
+        'manage' => 'iredeems::items.manage resource',
         'index' => 'iredeems::items.list resource',
         'create' => 'iredeems::items.create resource',
         'edit' => 'iredeems::items.edit resource',
         'destroy' => 'iredeems::items.destroy resource',
     ],
     'iredeems.redeems' => [
+        'manage' => 'iredeems::redeems.manage resource',
         'index' => 'iredeems::redeems.list resource',
         'create' => 'iredeems::redeems.create resource',
         'edit' => 'iredeems::redeems.edit resource',
         'destroy' => 'iredeems::redeems.destroy resource',
     ],
     'iredeems.pointhistories' => [
+        'manage' => 'iredeems::pointhistories.manage resource',
         'index' => 'iredeems::pointhistories.list resource',
         'create' => 'iredeems::pointhistories.create resource',
         'edit' => 'iredeems::pointhistories.edit resource',
         'destroy' => 'iredeems::pointhistories.destroy resource',
     ],
     'iredeems.points' => [
+        'manage' => 'iredeems::point.manage resource',
         'index' => 'iredeems::points.list resource',
         'create' => 'iredeems::points.create resource',
         'edit' => 'iredeems::points.edit resource',
